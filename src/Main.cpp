7f770dd72b3aafd28e747e5530fd0dce6df75487
Ryan Purse <ryanpurse@gmail.com>
2022-02-06 16:49:38 +0000

    Updated CMakeLists.txt to use other CMakes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,22 @@
 /**
- * @brief
- * @return
+ * @file Main.cpp
+ * @date 06/02/2022
+ * @author Ryan Purse
  */
+
+#include "Ecs.h"
+#include "glm.hpp"
+
+
 int main()
 {
+    ecs::init();
+    
+    ecs::create<glm::vec2>(ecs::TypeDefault);
+    
+    ecs::Entity e = ecs::create();
+    ecs::add(e, glm::vec2(14.f, 10.f));
+    
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
