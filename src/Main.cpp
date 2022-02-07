8b68b275d1e86ed1683d0416b12c0ca395bd3ac5
Ryan Purse <ryanpurse@gmail.com>
2022-02-07 10:15:17 +0000

    Added glfw as a submodule.

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,10 +7,29 @@
 #include "Ecs.h"
 #include "glm.hpp"
 #include "glew.h"
+#include "glfw3.h"
 
 int main()
 {
     ecs::init();
+    
+    if (!glfwInit())
+        return 3;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);  // Version of OpenGl you want to use.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+    
+    auto *window = glfwCreateWindow(1920, 1080, "e", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return 3;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
     if (glewInit() != GLEW_OK)
     {
         // No context yet.
