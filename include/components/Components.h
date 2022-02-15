68878bfdc52860541a0c7b09b767a50d057716a1
Ryan Purse <ryanpurse@gmail.com>
2022-02-15 10:16:51 +0000

    Refactored slightly.

--- include/components/Components.h
+++ include/components/Components.h
@@ -12,17 +12,36 @@
 #include "glm.hpp"
 #include "detail/type_quat.hpp"
 
+/** Core elements used for rendering an item to the screen by OpenGL. ONLY items that never change each frame. E.g.: No uniforms */
+struct RenderCoreElements;
+
+/** Vertex Buffer Object. Stores vertex data on the GPU. */
+struct Vbo;
+
+/** Element Buffer Object. Tells the GPU what order each vertex should be rendered. */
+struct Ebo;
+
+/** Holds the position, orientation and scale of an object. Note: this should be split to improve cache miss optimisation. */
+struct Transform;
+
+/** Holds information about how to rotate an object. */
+struct Rotator;
+
+/** Uniforms used when dealing with a basic mesh */
+struct BasicUniforms;
+
+/** A basic vertex */
+struct BasicVertex;
+
+/** A basic mesh made of basic vertices */
+struct BasicMesh;
+
 struct RenderCoreElements
 {
     unsigned int    vao      { 0 };
     unsigned int    fbo      { 0 };
     int             eboCount { 0 };
     // Matrix Uniforms are not included since they need to be updated every frame.
-};
-
-struct Vao
-{
-    unsigned int id;
 };
 
 struct Vbo
@@ -35,16 +54,6 @@ struct Ebo
     unsigned int id;
 };
 
-struct EboCount
-{
-    int count;
-};
-
-struct Fbo
-{
-    unsigned int id { 0 };
-};
-
 struct Transform
 {
     glm::vec3 position { 0.f };
@@ -54,9 +63,9 @@ struct Transform
 
 struct Rotator
 {
-    float time { 0.f };
-    float height { 0.f };
-    glm::vec3 originalHeight { 0.f };
+    float       time            { 0.f };
+    float       height          { 0.f };
+    glm::vec3   originalHeight  { 0.f };
 };
 
 struct BasicUniforms
@@ -64,10 +73,7 @@ struct BasicUniforms
     glm::mat4 modelMat { 1.f };
 };
 
-// struct Vertex {};
-
 struct BasicVertex
-        // : Vertex
 {
     glm::vec3 position  { 0.f };
     glm::vec3 colour    { 0.f };
